294899c33fa99752f3852c7f98574978c49917f1
Wilgot Wardell <wilgotwardell@hotmail.com>
2022-10-28 11:58:53 +0200

    -

--- BokaPlats/Program.cs
+++ BokaPlats/Program.cs
@@ -2,20 +2,43 @@
 {
     internal class Program
     {
-
+        
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("\nVälkommen till SJ-bokning! \n");
+
+            Menu();
+
+            Console.ReadLine();
+
+        }
+        static void Menu()
+        {
+            string answer;
             Console.WriteLine("Vad vill du göra: ");
             Console.WriteLine("\u2022 Boka, skriv \"B\", på samma rad följt av ett platsnummer.");
             Console.WriteLine("\u2022 Avboka, skriv \"A\" på samma rad följt av ett platsnummer. ");
             Console.WriteLine("\u2022 Skriva ut det sena\"S\"ste bokade biljetterna, skriv \"S\"");
             Console.WriteLine("\u2022 Avsluta, skriv \"Q\"");
 
-
-            Console.ReadLine();
-
+            try
+            {
+                answer = Console.ReadLine().ToLower();               
+                char firstLetter = answer[0];
+                if(firstLetter == 'b')
+                {
+                    Console.WriteLine("Bokar");
+                }
+                else
+                {
+                    Console.WriteLine("hfjf");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
         }
     }
 }
