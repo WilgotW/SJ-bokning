c4223ee469c0adfc8190618267b1363dfefbf95a
Wilgot Wardell <wilgotwardell@hotmail.com>
2022-10-28 11:49:30 +0200

    first commit

---
+++ BokaPlats/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BokaPlats
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("\nVälkommen till SJ-bokning! \n");
+            Console.WriteLine("Vad vill du göra: ");
+            Console.WriteLine("\u2022 Boka, skriv \"B\", på samma rad följt av ett platsnummer.");
+            Console.WriteLine("\u2022 Avboka, skriv \"A\" på samma rad följt av ett platsnummer. ");
+            Console.WriteLine("\u2022 Skriva ut det sena\"S\"ste bokade biljetterna, skriv \"S\"");
+            Console.WriteLine("\u2022 Avsluta, skriv \"Q\"");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
